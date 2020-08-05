fa0b78c50a7363025be0eb76eefd3ec76e268d68
Zac Smith <zrs3141592@gmail.com>
2020-08-05 11:10:10 -0600

    set up solutions file

---
+++ solutions/index.js
@@ -0,0 +1,15 @@
+// 1. Create a function FirstReverse() that takes a string as parameter and returns the str in reverse order. Review array functions: split, reverse and join
+
+// Create function here
+const FirstReverse = (str) => {}
+
+// console.log(`Q1: ${FirstReverse('test')}`)
+
+// 2. Create a function FirstFactorial() that takes a number as a parameter and returns the factorial of it. IE number =4, return (4*3*2*1)
+
+const FirstFactorial = (num) => {}
+
+module.exports = {
+  FirstReverse,
+  FirstFactorial,
+}
